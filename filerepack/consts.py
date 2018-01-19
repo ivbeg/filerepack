26d72c171bf382d07232ef1f573421d4470d0eab
Иван Бегтин <ibegtin@gmail.com>
2018-01-19 14:00:28 +0300

    Added more Microsoft OOXML formats as supported exts

--- filerepack/consts.py
+++ filerepack/consts.py
@@ -1,15 +1,34 @@
 #-*- coding: utf-8 -*-
 
+# Compression options
 ZIP_PATH = 'zip'
 SZIP_PATH = '7za'
 SZIP_OPTIONS = ''
 
+# Compression options
 DEFAULT_JPEG_QUALITY = '85'
 JPEGOPTIM_PATH = 'jpegoptim'
 JPEGIOPTIM_OPTIONS = ' --strip-all -m%s -p -o ' % (DEFAULT_JPEG_QUALITY)
 PNGQUANT_PATH = 'pngquant'
 PNGQUANT_OPTIONS = ' --force --speed 1 '
-SUPPORTED_EXTS = ['docx', 'pptx', 'xlsx', 'ods', 'odt', 'otp','ott', 'ppsx', 'xmind', 'epub', 'zip', 'jar', 'apk', 'ipa', 'sldx']
+
+SUPPORTED_EXTS = ["zip",
+# Microsoft OOXML zip based formats
+'accdt', 'crtx', 'docm', 'docx', 'dotm', 'dotx', 'gcsx', 'glox', 'gqsx',
+'potm', 'potx', 'ppam', 'ppsm', 'ppsx', 'pptm', 'pptx',
+'sldx', 'thmx', 'vdw', 'xlam', 'xlsb', 'xlsm', 'xlsx', 'xltm', 'xltx', 'zipx',
+# OpenXML file formats
+'ods', 'odt', 'otp','ott',
+# MindMaps
+'xmind',
+# EBooks
+'epub',
+# Programming packages
+'jar', 'egg',
+# Android and iPhone apps
+'apk', 'ipa']
+
+
 EXT_IMAGE_MAP = {
     'docx': ['/word/media', '/docProps'],
     'xlsx': ['/xl/media', '/docProps'],
@@ -29,4 +48,6 @@ EXT_IMAGE_MAP = {
 }
 
 # File types with this extension are sensitive to ZIP program used. We use "zip" instead of 7-Zip to improve it's compression
-ZIP_SENSITIVE_EXTS = ['docx', 'pptx', 'xlsx', 'ppsx', 'pptx']
+ZIP_SENSITIVE_EXTS = ['accdt', 'crtx', 'docm', 'docx', 'dotm', 'dotx', 'gcsx', 'glox', 'gqsx',
+'potm', 'potx', 'ppam', 'ppsm', 'ppsx', 'pptm', 'pptx',
+'sldx', 'thmx', 'vdw', 'xlam', 'xlsb', 'xlsm', 'xlsx', 'xltm', 'xltx', 'zipx']
